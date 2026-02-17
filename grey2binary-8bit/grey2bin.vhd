62e74d9be4c589655f459b384829ab87928c1dc6
Vaibhav Gupta <vaibhavgupta6393@gmail.com>
2026-02-17 11:45:24 +0530

    added comments for better readability and understanding of the code.

--- grey2binary-8bit/grey2bin.vhd
+++ grey2binary-8bit/grey2bin.vhd
@@ -1,25 +1,19 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+library IEEE; -- Include the IEEE library for standard logic definitions
+use IEEE.STD_LOGIC_1164.ALL; -- Use the standard logic package from the IEEE library to define the types and operations for std_logic and std_logic_vector
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+entity grey2binary is -- Define the entity for the gray to binary converter
+-- Define the ports for the entity
+    Port ( G : in STD_LOGIC_VECTOR (7 downto 0); -- Input port for the 8-bit gray code
+           B : inout STD_LOGIC_VECTOR (7 downto 0)); -- Inout port for the 8-bit binary output (can be used as input or output)
+end grey2binary; -- End of the entity declaration
 
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
-entity grey2binary is
-    Port ( G : in STD_LOGIC_VECTOR (7 downto 0);
-           B : inout STD_LOGIC_VECTOR (7 downto 0));
-end grey2binary;
-
-architecture Behavioral of grey2binary is
+architecture Behavioral of grey2binary is -- Define the architecture for the gray to binary converter
 
 begin
 
-B(7) <= G(7);
+B(7) <= G(7); -- The MSB of the binary output is the same as the MSB of the gray code input
+
+-- The next bit of the binary output is the XOR of the previous binary bit and the corresponding gray code bit
 B(6) <= B(7) xor G(6);
 B(5) <= B(6) xor G(5);
 B(4) <= B(5) xor G(4);
@@ -28,4 +22,4 @@ B(2) <= B(3) xor G(2);
 B(1) <= B(2) xor G(1);
 B(0) <= B(1) xor G(0);
 
-end Behavioral;
+end Behavioral; -- End of the architecture declaration
